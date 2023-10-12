9b6671575b46548b6606e99fef36189ef8162278
Pranav Sharma <119229843+ItsPranavz@users.noreply.github.com>
2023-10-13 00:10:35 +0530

    Added CS1301-A4-(A).cpp for assignment 4 part A.

---
+++ CS1301-A4-(A).cpp
@@ -0,0 +1,104 @@
+#include <iostream>  
+using namespace std;  
+
+class Node {  
+public:  
+  int data;  
+  Node *next;  
+  
+  Node(int data) {  
+    this->data = data;  
+    this->next = nullptr;  
+  }  
+};  
+
+class LinkedList {  
+private:  
+  Node *head;  
+  
+public:  
+  LinkedList() {  
+    this->head = nullptr;  
+  }  
+  
+  void insertAtBeginning(int data) {  
+    Node *newNode = new Node(data);  
+    newNode->next = head;  
+    head = newNode;  
+  }  
+  
+  void insertAtEnd(int data) {  
+    Node *newNode = new Node(data);  
+    if (head == nullptr) {  
+      head = newNode;  
+      return;  
+    }  
+    Node *temp = head;  
+    while (temp->next != nullptr) {  
+      temp = temp->next;  
+    }  
+    temp->next = newNode;  
+  }  
+  
+  void deleteAtBeginning() {  
+    if (head == nullptr) {  
+      return;  
+    }  
+    Node *temp = head;  
+    head = head->next;  
+    delete temp;  
+  }  
+  
+  void deleteAtEnd() {  
+  if (head == nullptr) {  
+    return;  
+  }  
+  if (head->next == nullptr) {  
+    delete head;  
+    head = nullptr;  
+    return;  
+  }  
+  Node *temp = head;  
+  while (temp->next->next != nullptr) {  
+    temp = temp->next;  
+  }  
+  delete temp->next;  
+  temp->next = nullptr;  
+}  
+  
+void printList() {  
+  Node *temp = head;  
+  while (temp != nullptr) {  
+    cout << temp->data << " ";  
+    temp = temp->next;  
+  }  
+  cout << endl;  
+}  
+};  
+  
+int main() {  
+LinkedList List;  
+
+list.insertAtBeginning(3);  
+list.insertAtBeginning(2);  
+list.insertAtBeginning(1);  
+
+list.insertAtEnd(4);  
+list.insertAtEnd(5);  
+list.insertAtEnd(6);  
+
+cout << "Original list: ";  
+list.printList();  
+
+list.deleteAtBeginning();  
+
+cout << "List after deleting a node at the beginning: ";  
+list.printList();  
+
+list.deleteAtEnd();  
+
+cout << "List after deleting a node at the end: ";  
+list.printList();  
+  
+return 0;  
+}
